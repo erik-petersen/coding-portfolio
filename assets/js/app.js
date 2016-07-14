948b2cc4caa68432c8ead7013286ce26999f4e8b
Erik Petersen <contacterikpetersen@gmail.com>
2016-07-14 13:45:50 -0400

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module( "myApp", ["ngRoute"] );
+
+myApp.config( function( $routeProvider ) {
+  $routeProvider
+  .when( "/home", {
+    templateUrl: "../../pages/home.html"
+  })
+  .when( "/portfolio", {
+    templateUrl: "pages/portfolio.html"
+  })
+  .when( "/about", {
+    templateUrl: "pages/about.html"
+  })
+  .when( "/blog", {
+    templateUrl: "pages/blog.html"
+  })
+  .otherwise({
+    redirectTo: "/home"
+  });
+}); // end myApp.config
