9c7659ac0284f8b6a0ac1f10873508c34d4398f6
Erik Petersen <contacterikpetersen@gmail.com>
2016-08-01 18:08:41 -0400

    Added user_avatar to localStorage and can display icon of user who created bill...but this is done solely on the back end

--- assets/js/headerController.js
+++ assets/js/headerController.js
@@ -2,7 +2,7 @@ myApp.controller( "HeaderController", [ "$scope", function( $scope ) {
 
   $( ".fa-bars" ).click( function() {
     console.log( "Hello" );
-    $( ".main-nav" ).toggle( "slide" );
+    $( ".main-nav" ).slideToggle();
   }); // end .fa-bars click event
 
 }]); // end HeaderController
