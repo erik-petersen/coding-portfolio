4e688870d44b14ceb0f87a789e7c5251bb796ee3
Erik Petersen <contacterikpetersen@gmail.com>
2016-08-16 15:15:48 -0400

    Fixed nav bar so it closes when an item is clicked

--- assets/js/headerController.js
+++ assets/js/headerController.js
@@ -2,7 +2,11 @@ myApp.controller( "HeaderController", [ "$scope", function( $scope ) {
 
   jQuery( ".fa-bars" ).click( function() {
     console.log( "Hello" );
-    jQuery( ".main-nav" ).slideToggle( 150 );
+    jQuery( ".main-nav" ).slideToggle( 250 );
   }); // end .fa-bars click event
+
+  jQuery( ".nav-item" ).click( function() {
+    jQuery( ".main-nav" ).slideToggle( 250 );
+  }); // end .nav-item click event
 
 }]); // end HeaderController
