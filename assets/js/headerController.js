43be267942f6079b78eb28164a41ddc4f4aa755c
Erik Petersen <contacterikpetersen@gmail.com>
2016-09-12 15:30:24 -0400

    Disabled nav slideToggle for desktop screens

--- assets/js/headerController.js
+++ assets/js/headerController.js
@@ -1,12 +1,17 @@
 myApp.controller( "HeaderController", [ "$scope", function( $scope ) {
 
-  jQuery( ".fa-bars" ).click( function() {
-    console.log( "Hello" );
-    jQuery( ".main-nav" ).slideToggle( 250 );
-  }); // end .fa-bars click event
+  // thank you, http://www.coalmarch.com/blog/how-to-execute-javascript-based-on-screen-size-using-jquery, for your help!!!
+  if( $( window ).width() < 744 ) {
 
-  jQuery( ".nav-item" ).click( function() {
-    jQuery( ".main-nav" ).slideToggle( 250 );
-  }); // end .nav-item click event
+    console.log( "Greater than 744" );
+    jQuery( ".fa-bars" ).click( function() {
+      console.log( "Hello" );
+      jQuery( ".main-nav" ).slideToggle( 250 );
+    }); // end .fa-bars click event
+
+    jQuery( ".nav-item" ).click( function() {
+      jQuery( ".main-nav" ).slideToggle( 250 );
+    }); // end .nav-item click event
+  }
 
 }]); // end HeaderController
