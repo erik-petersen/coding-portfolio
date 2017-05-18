6621611c65de1951506e23be83d5376bb415e511
Erik Petersen <contacterikpetersen@gmail.com>
2017-05-17 20:22:33 -0400

    Fixed hamburger functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,26 @@
+$( function() {
 
+  // "use strict";
+  //
+  // var clicks = 0;
+  //
+  // $( ".fa-bars" ).focusin( function() {
+  //
+  //   clicks++;
+  //
+  //   if( clicks == 1 ) {
+  //     $( ".navbar-collapse.collapse" ).show();
+  //     console.log( clicks );
+  //   }
+  //
+  //   else {
+  //     console.log( clicks );
+  //     $( ".navbar-collapse.collapse" ).hide();
+  //
+  //     clicks = 0;
+  //   }
+  //
+  //
+  // });
+
+}); // end outmost function
