a43db26ec9004a20e4727f5a68c6d48de2b38ed8
Erik Petersen <contacterikpetersen@gmail.com>
2017-05-17 21:52:27 -0400

    Tweaking nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,26 +1,28 @@
 $( function() {
 
-  // "use strict";
-  //
-  // var clicks = 0;
-  //
-  // $( ".fa-bars" ).focusin( function() {
-  //
-  //   clicks++;
-  //
-  //   if( clicks == 1 ) {
-  //     $( ".navbar-collapse.collapse" ).show();
-  //     console.log( clicks );
-  //   }
-  //
-  //   else {
-  //     console.log( clicks );
-  //     $( ".navbar-collapse.collapse" ).hide();
-  //
-  //     clicks = 0;
-  //   }
-  //
-  //
+  "use strict";
+
+  var clicks = 0;
+
+  // $( ".fa-bars" ).click( function() {
+
+    clicks++;
+
+    // $( ".navbar-collapse.collapse" ).toggle();
+
+    if( clicks == 1 ) {
+      $( ".nav-element" ).css( "display", "block" );
+      console.log( clicks );
+    }
+
+    else {
+      console.log( clicks );
+      // $( ".navbar-collapse.collapse" ).hide();
+
+      clicks = 0;
+    }
+
+
   // });
 
 }); // end outmost function
